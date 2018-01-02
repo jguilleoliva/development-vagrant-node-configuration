9e3e8eda73fd9080b6d2f70c7b82086189e7fd9f
Juan Guillermo Oliva <jguilleoliva@gtxsolutions.com>
2018-01-01 23:02:05 -0600

    #commit-report | made it easier for new install to get all up and running, by adding git repo and auto-pull from pre-install-script.sh

--- scripts/pre-install-script.sh
+++ scripts/pre-install-script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "#--- PREPARING-DATA-FOLDERS ---#"
-mkdir ../data
-mkdir ../data/html
-mkdir ../data/mysql
+mkdir ../data;
+mkdir ../data/html;
+mkdir ../data/mysql;
+echo "#--- INITIALIZING-GIT-REPO ---#"
+cd ../data/html;
+git init;
+git remote add origin git@bitbucket.org:astegconsultores/framework-nine-web.git;
+git pull origin master;
 echo "#--- PROCESS-COMPLETED ---#"
