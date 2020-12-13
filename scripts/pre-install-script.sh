f65027145555314a2331337eb3bd10a3c6bcfbea
Juan Oliva <jguilleoliva@astegconsultores.com>
2020-12-13 16:38:18 -0600

    new-version-upgrade

--- scripts/pre-install-script.sh
+++ scripts/pre-install-script.sh
@@ -6,6 +6,6 @@ mkdir ../data/html;
 echo "#--- INITIALIZING-GIT-REPO ---#"
 cd ../data/html;
 git init;
-git remote add origin-baseline git@bitbucket.org:astegconsultores/baseline-framework-ninesix.git;
+git remote add origin-baseline git@github.com:jguilleoliva/asteg-framework.git;
 git pull origin-baseline master;
 echo "#--- PROCESS-COMPLETED ---#"
